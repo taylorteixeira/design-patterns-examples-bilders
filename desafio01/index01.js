cfd076636085d755926e706b90fb0019bbf7ed12
Taylor Vargas Teixeira <taylorvargasteixeira@gmail.com>
2025-09-18 18:26:20 -0300

    Padrão01 Bilder

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,48 @@
-console.log("Hello, World! 001");
+// Exemplo do padrão Builder
+class LancheBuilder {
+  constructor() {
+    this.lanche = {}
+  }
+
+  adicionarPao(tipo) {
+    this.lanche.pao = tipo
+    return this
+  }
+
+  adicionarCarne(tipo) {
+    this.lanche.carne = tipo
+    return this
+  }
+
+  adicionarQueijo(tipo) {
+    this.lanche.queijo = tipo
+    return this
+  }
+
+  adicionarSalada(tipo) {
+    this.lanche.salada = tipo
+    return this
+  }
+
+  adicionarMolho(tipo) {
+    this.lanche.molho = tipo
+    return this
+  }
+
+  construir() {
+    return this.lanche
+  }
+}
+
+// Uso do Builder
+const builder = new LancheBuilder()
+
+const lanchePersonalizado = builder
+  .adicionarPao("Integral")
+  .adicionarCarne("Frango")
+  .adicionarQueijo("Cheddar")
+  .adicionarSalada("Alface e Tomate")
+  .adicionarMolho("Maionese")
+  .construir()
+
+console.log("Lanche personalizado:", lanchePersonalizado)
